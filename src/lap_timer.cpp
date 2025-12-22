587929308a873159058a8be0d38bf558b8635025
Blazu6 <114349938+Blazu6@users.noreply.github.com>
2025-12-22 18:37:25 +0100

    Enhance lap timer with file logging and velocity data
    - NOTE: Log file paths are currently hardcoded to local user directory - needs to be parameterized in the future.
    - Main publisher is currently set to debug mode/test values.

--- src/lap_timer.cpp
+++ src/lap_timer.cpp
@@ -3,10 +3,15 @@
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/u_int16.hpp"
 #include "geometry_msgs/msg/vector3.hpp"
-#include "putm_vcl_interfaces/msg/lap_timer.hpp"
+#include "geometry_msgs/msg/twist_with_covariance_stamped.hpp"
+// #include "putm_vcl_interfaces/msg/lap_timer.hpp"
 #include <cmath>
 #include <chrono>
-
+#include <sstream> // Do budowania nazwy pliku
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <ctime>
 // Use the chrono_literals namespace for easier time manipulation
 using namespace std::chrono_literals;
 
@@ -22,24 +27,63 @@ public:
             "/vectornav/gnss", 50, std::bind(&LapTimer::gps_callback, this, std::placeholders::_1));
 
         // Create publishers for the lap timer delta and time
-        lap_timer_pub = this->create_publisher<putm_vcl_interfaces::msg::LapTimer>("/putm_vcl/lap_timer", 50);
+        // lap_timer_pub = this->create_publisher<putm_vcl_interfaces::msg::LapTimer>("/putm_vcl/lap_timer", 50);
 
         // Create a timer that triggers every 20 milliseconds
         timer_ = this->create_wall_timer(
             20ms, std::bind(&LapTimer::lap_timer_callback, this));
+        //creata a subscription to the "/vectronav/velocity_body" topic with a QoS of 50
+        body_velocity_sub= this->create_subscription<geometry_msgs::msg::TwistWithCovarianceStamped>(
+            "/vectornav/velocity_body", 10,std::bind(&LapTimer::vel_bd_callback, this, std::placeholders::_1));
+        //create csv file and trigger a header
+        auto t = std::time(nullptr);
+        auto tm = *std::localtime(&t);
+
+        //create file path with time included
+        std::stringstream ss;
+        ss << "/home/franiuu/PUTM_VP_LAPTIMER/log/laptimer_data_" 
+        << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S")
+        << ".csv";
+        std::stringstream ss_constant;
+        ss_constant << "/home/franiuu/PUTM_VP_LAPTIMER/log/laptimer_data_we_"
+        << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S")
+        <<".csv";
+
+        std::string file_path_for_const=ss_constant.str();
+        std::string file_path = ss.str();
+
+        // open file
+        file_.open(file_path);
+        file_ << std::fixed << std::setprecision(10);
+        file_const.open(file_path_for_const);
+        file_const<< std::fixed << std::setprecision(10);
+        //log if file is open or closed
+        if(!file_.is_open()) {
+            RCLCPP_ERROR(this->get_logger(), "Could not open file: %s", file_path.c_str());
+        } 
+        else {
+            RCLCPP_INFO(this->get_logger(), "Log file created: %s", file_path.c_str());
+        }
     }
 
 private:
+
+    std::ofstream file_;
+    std::ofstream file_const;
+    std::string file_path_;
     // Timer object
     rclcpp::TimerBase::SharedPtr timer_;
 
     // Subscription to the GPS topic
     rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_sub_;
 
+    //Subscription to the Velocity body topic
+    rclcpp::Subscription<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr body_velocity_sub;
+    
     // Publishers for the lap timer delta and time
     rclcpp::Publisher<std_msgs::msg::UInt16>::SharedPtr lap_timer_delta_pub_;
     rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr lap_timer_time_pub_;
-    rclcpp::Publisher<putm_vcl_interfaces::msg::LapTimer>::SharedPtr lap_timer_pub;
+    // rclcpp::Publisher<putm_vcl_interfaces::msg::LapTimer>::SharedPtr lap_timer_pub;
 
     // Last known latitude and longitude
     double last_lat, last_lon;
@@ -73,9 +117,12 @@ private:
 
     // Start/finish line coordinates
     const double EARTH_RADIUS = 6371000.0; // Earth's radius in meters
-    const double START_LAT = 52.239040;    // Start latitude
-    const double START_LON = 16.229503;    // Start longitude
+    const double START_LAT = 52.239048;    // Start latitude
+    const double START_LON = 16.230333;    // Start longitude
     const double DELTA_DISTANCE = 0.5;     // Minimum distance between sectors
+
+    //create every single point marker
+    int current_checkpoint = 0;
 
     // Structure to represent a sector
     struct Sector
@@ -97,7 +144,7 @@ private:
 
     // Function to convert degrees to radians
     double degreesToRadians(double degrees)
-    {
+    {  
         // Convert degrees to radians using the formula: radians = degrees * pi / 180
         return degrees * M_PI / 180.0;
     }
@@ -124,6 +171,13 @@ private:
         // Return the distance in meters
         return EARTH_RADIUS * c;
     }
+    //spped callback function
+    double current_spd=0.0;
+    void vel_bd_callback(const geometry_msgs::msg::TwistWithCovarianceStamped::SharedPtr msg)
+    {
+        current_spd = msg->twist.twist.linear.x;
+    }
+
 
     // Callback function for the GPS subscription
     void gps_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg)
@@ -131,7 +185,7 @@ private:
         // Get the current latitude and longitude
         double current_lat = msg->latitude;
         double current_lon = msg->longitude;
-
+        
         // Get the current time
         rclcpp::Time now = this->now();
 
@@ -163,8 +217,32 @@ private:
                     }
                     lap_count++;
                     sector_number = 0;
-                    
 
+
+                    // create current checkpoint and make new csv file which will make data for every single lap
+                    auto t = std::time(nullptr);
+                    auto tm = *std::localtime(&t);
+
+                    std::stringstream ss;
+                    ss << "/home/franiuu/PUTM_VP_LAPTIMER/log/laptimer_data_"
+                    << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S")
+                    << ".csv";
+                    if (file_.is_open()){
+                        file_.close();
+                    }
+                    file_path_ = ss.str();
+
+                    file_.open(file_path_);
+                    file_ << std::fixed << std::setprecision(10);
+
+                    if (!file_.is_open()) {
+                        RCLCPP_ERROR(this->get_logger(), "Could not open file: %s", file_path_.c_str());
+                    } else {
+                        RCLCPP_INFO(this->get_logger(), "Log file created: %s", file_path_.c_str());
+                        file_ << "checkpoint,delta,lat,lon,lap,speed\n";
+                    }
+
+  
                     // Update the best lap time if necessary
                     switch (lap_count)
                     {
@@ -247,25 +325,36 @@ private:
         // Update the last known latitude and longitude
         last_lat = current_lat;
         last_lon = current_lon;
+        RCLCPP_INFO(this->get_logger(), "Delta:  %.3f, Current lap: %d, Best lap: %d, Lap count: %d\n", delta_time, lt, blt, lap_count);
+        //draw data for laptimer
+        if (file_.is_open())
+        {
+            current_checkpoint ++;
+            file_const << current_checkpoint << "," << delta_time << "," << current_lat << "," << current_lon << "," << lap_count << "," << current_spd <<"\n";
+            file_ << current_checkpoint << "," << delta_time << "," << current_lat << "," << current_lon << "," << lap_count << "," << current_spd <<"\n";
+            file_const.flush();
+            file_.flush();
+        }
     }
 
     // Callback function for the lap timer
     void lap_timer_callback()
     {
         // Publish the delta time
-        auto message = putm_vcl_interfaces::msg::LapTimer();
+        // auto message = putm_vcl_interfaces::msg::LapTimer();
         double del = 0.2322;
         // int16_t u = static_cast<int16_t>(del * 1000);
         int16_t u = -2137;
-        message.delta = (int16_t)delta_time;
-        message.current_lap = (uint16_t)lt;
-        message.best_lap = (uint16_t)blt;
-        message.lap_counter = (uint8_t)lap_count;
-        message.current_lap = 56145;
-        message.best_lap = 52123;
-        message.lap_counter = 11;
-        message.delta = u;
-        lap_timer_pub->publish(message);
+        // message.delta = (int16_t)delta_time;
+        // message.current_lap = (uint16_t)lt;
+        // message.best_lap = (uint16_t)blt;
+        // message.lap_counter = (uint8_t)lap_count;
+        // message.current_lap = 56145;
+        // message.best_lap = 52123;
+        // message.lap_counter = 11;
+        // message.delta = u;
+        // lap_timer_pub->publish(message);
+        
     }
 };
 
