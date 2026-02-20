4756c0a8e040d46f8be060f68cce29e3a752e110
Blazu6 <blazejczykolaf@gmail.com>
2026-02-20 18:15:28 +0100

    feat: integrate state machine with robust CSV file logging
    - Implemented std::filesystem for automatic log directory creation and safe path handling.

--- src/lap_timer.cpp
+++ src/lap_timer.cpp
@@ -3,11 +3,17 @@
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/u_int16.hpp"
 #include "geometry_msgs/msg/vector3.hpp"
+#include "geometry_msgs/msg/twist_with_covariance_stamped.hpp"
 #include <cmath>
 #include <chrono>
 #include <memory>
 #include <sstream>
 #include <array>
+#include <fstream>
+#include <ctime>
+#include <iomanip>
+#include <filesystem>
+#include <cstdlib>
 
 using namespace std::chrono_literals;
 
@@ -25,40 +31,90 @@ private:
 public:
     LapTimer() : Node("lap_timer"), m_state(State::WAITING_FOR_START)
     {
-        // Declare use_sim_time parameter
-        if (!this->has_parameter("use_sim_time"))
-        {
-            this->declare_parameter("use_sim_time", rclcpp::ParameterValue(false));
-        }
-        
-        // Check and log whether sim time is being used
-        if (this->get_parameter("use_sim_time").as_bool()) {
-            RCLCPP_INFO(this->get_logger(), "Using Simulation Time");
-        } else {
-            RCLCPP_INFO(this->get_logger(), "Using System Time (Wall Time). For bag playback, use --ros-args -p use_sim_time:=true");
-        }
-
         // ROS2 Subscribers and Publishers
         gps_sub_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(
             "/vectornav/gnss", 50, std::bind(&LapTimer::gps_callback, this, std::placeholders::_1));
+        //creata a subscription to the "/vectronav/velocity_body" topic with a QoS of 50
+        body_velocity_sub= this->create_subscription<geometry_msgs::msg::TwistWithCovarianceStamped>(
+             "/vectornav/velocity_body", 10,std::bind(&LapTimer::vel_bd_callback, this, std::placeholders::_1));
 
         lap_timer_pub_ = this->create_publisher<std_msgs::msg::String>("/putm_vcl/lap_timer", 50);
 
         // ROS2 Wall timer for periodic publishing
         m_wall_timer = this->create_wall_timer(
             20ms, std::bind(&LapTimer::lap_timer_callback, this));
-        
+
         // Initial gate post calculation for the start line
         setup_gate_posts(START_CENTER_LAT, START_CENTER_LON, START_HEADING_DEG, 10.0);
+
+        const char* home_dir = std::getenv("HOME");
+        std::string base_path_str;
+
+        if (home_dir) 
+        {
+            base_path_str = std::string(home_dir) + "/PUTM_VP_LAPTIMER/csv_logs/";
+        } 
+        else 
+        {
+            base_path_str = "/tmp/PUTM_VP_LAPTIMER/csv_logs/";
+        }
+
+        std::filesystem::path log_dir(base_path_str);
+
+        RCLCPP_INFO(this->get_logger(), "Log directory path: %s", log_dir.string().c_str());
+        try {
+                if (!std::filesystem::exists(log_dir)) {
+                    std::filesystem::create_directories(log_dir);
+                    RCLCPP_INFO(this->get_logger(), "Created log directory: %s", log_dir.string().c_str());
+                }
+                else { 
+                    RCLCPP_INFO(this->get_logger(), "Directory already exists: %s", log_dir.string().c_str());
+                }
+            } catch (const std::exception& e) {
+                RCLCPP_ERROR(this->get_logger(), "Error creating log directory: %s", e.what());
+            }
+        auto t = std::time(nullptr);
+        auto tm = *std::localtime(&t);
+        
+        //stworzenie drugiej nazwy pliku z aktualną datą i godziną dla całej sesji
+        std::stringstream ss_filename_session;
+        ss_filename_session << "SESSION_TOTAL_" << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S") << ".csv";
+        std::filesystem::path full_path_const = log_dir / ss_filename_session.str();
+
+        log_dir_ = log_dir;
+        session_file_.open(full_path_const);
+
+        if (session_file_.is_open()) {
+            session_file_ << std::fixed << std::setprecision(10);
+            session_file_ << "timestamp,lap_count,lat,lon,speed,state\n";
+            RCLCPP_INFO(this->get_logger(), "Session log file created: %s", full_path_const.string().c_str());
+        } else {
+            RCLCPP_ERROR(this->get_logger(), "Could not open session file: %s", full_path_const.string().c_str());
+        }
 
         RCLCPP_INFO(this->get_logger(), "LapTimer initialized, waiting for first start line cross.");
     }
 
+    ~LapTimer() 
+    {
+        if (lap_file_.is_open()) lap_file_.close();
+        if (session_file_.is_open()) session_file_.close();
+        RCLCPP_INFO(this->get_logger(), "Log files closed safely.");
+    }
+
 private:
+    std::filesystem::path log_dir_;
+    std::ofstream lap_file_;
+    std::ofstream session_file_;  
+
     // -- ROS2 Constructs --
     rclcpp::TimerBase::SharedPtr m_wall_timer;
     rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_sub_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr lap_timer_pub_;
+
+    rclcpp::Subscription<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr body_velocity_sub;
+
+    double current_spd=0.0;
 
     // -- State Machine --
     State m_state;
@@ -190,39 +246,58 @@ private:
             m_current_lap_start_time = now;
             m_state = State::RECORDING_REFERENCE_LAP;
             RCLCPP_INFO(this->get_logger(), "Crossed start line for the first time. Starting lap 1.");
-            return; // Nothing more to do
         }
-
-        // If we are here, a lap has been completed.
-        double completed_lap_time_s = (now - m_current_lap_start_time).seconds();
-        m_last_lap_time_ms = static_cast<uint32_t>(completed_lap_time_s * 1000);
-        
-        RCLCPP_INFO(this->get_logger(), "Lap %d finished. Time: %.3f s", m_lap_count, completed_lap_time_s);
-
-        bool is_new_best = false;
-
-        if (m_state == State::RECORDING_REFERENCE_LAP)
+        else 
         {
-            is_new_best = true; // First lap is always the best lap
-            m_state = State::LAPPING; // Transition to lapping state after recording the reference lap
+            // If we are here, a lap has been completed.
+            double completed_lap_time_s = (now - m_current_lap_start_time).seconds();
+            m_last_lap_time_ms = static_cast<uint32_t>(completed_lap_time_s * 1000);
+            
+            RCLCPP_INFO(this->get_logger(), "Lap %d finished. Time: %.3f s", m_lap_count, completed_lap_time_s);
+
+            bool is_new_best = false;
+
+            if (m_state == State::RECORDING_REFERENCE_LAP)
+            {
+                is_new_best = true; // First lap is always the best lap
+                m_state = State::LAPPING; // Transition to lapping state after recording the reference lap
+            }
+            else if (m_state == State::LAPPING && m_last_lap_time_ms < m_best_lap_time_ms)
+            {
+                is_new_best = true;
+            }
+
+            if (is_new_best)
+            {
+                m_best_lap_time_ms = m_last_lap_time_ms;
+                m_best_sectors_count = m_ref_sectors_count; // Update best sectors count
+                std::copy(m_reference_lap_sectors.begin(), m_reference_lap_sectors.begin() + m_ref_sectors_count, m_best_lap_sectors.begin());
+                RCLCPP_INFO(this->get_logger(), "New best lap recorded with time: %.3f s", completed_lap_time_s);
+            }
+
+            // Prepare for the next lap
+            m_lap_count++;
+            m_current_lap_start_time = now;
+            m_ref_sectors_count = 0;
         }
-        else if (m_state == State::LAPPING && m_last_lap_time_ms < m_best_lap_time_ms)
-        {
-            is_new_best = true;
+        if (lap_file_.is_open()) {
+            lap_file_.close();
         }
 
-        if (is_new_best)
-        {
-            m_best_lap_time_ms = m_last_lap_time_ms;
-            m_best_sectors_count = m_ref_sectors_count; // Update best sectors count
-            std::copy(m_reference_lap_sectors.begin(), m_reference_lap_sectors.begin() + m_ref_sectors_count, m_best_lap_sectors.begin());
-            RCLCPP_INFO(this->get_logger(), "New best lap recorded with time: %.3f s", completed_lap_time_s);
-        }
+        auto t = std::time(nullptr);
+        auto tm = *std::localtime(&t);
+        std::stringstream ss;
+        ss << "LAP_" << std::setw(2) << std::setfill('0') << static_cast<int>(m_lap_count) 
+        << "_" << std::put_time(&tm, "%H-%M-%S") << ".csv";
 
-        // Prepare for the next lap
-        m_lap_count++;
-        m_current_lap_start_time = now;
-        m_ref_sectors_count = 0;
+        std::filesystem::path new_lap_path = log_dir_ / ss.str();
+        lap_file_.open(new_lap_path.string());
+
+        if (lap_file_.is_open()) {
+            lap_file_ << std::fixed << std::setprecision(10);
+            lap_file_ << "time_into_lap,lat,lon,speed,delta\n"; // Nagłówek dla nowego pliku
+            RCLCPP_INFO(this->get_logger(), "Started logging new lap file: %s", ss.str().c_str());
+        }
     }
 
     void state_recording_reference_lap(double current_lat, double current_lon, const rclcpp::Time &now)
@@ -330,6 +405,11 @@ private:
     }
 //Main callback for GPS data, handling state transitions and lap timing logic
 
+    void vel_bd_callback(const geometry_msgs::msg::TwistWithCovarianceStamped::SharedPtr msg)
+    {
+        current_spd = msg->twist.twist.linear.x;
+    }
+
     void gps_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg)
     {
         double current_lat = msg->latitude;
@@ -358,6 +438,23 @@ private:
                     //m_delta_time_s, m_last_lap_time_ms, m_best_lap_time_ms, m_lap_count);
         m_prev_lat = current_lat;
         m_prev_lon = current_lon;
+
+        if (session_file_.is_open()) 
+        {
+        session_file_ << now.seconds() << "," 
+                      << static_cast<int>(m_lap_count) << "," 
+                      << current_lat << "," << current_lon << "," 
+                      << current_spd << "," << static_cast<int>(m_state) << "\n";
+        session_file_.flush();
+        }
+
+        if (lap_file_.is_open() && m_state != State::WAITING_FOR_START) 
+        {
+            lap_file_ << (now - m_current_lap_start_time).seconds() << "," 
+                    << current_lat << "," << current_lon << "," 
+                    << current_spd << "," << m_delta_time_s << "\n";
+            lap_file_.flush();
+    }
     }
 
     void lap_timer_callback()
